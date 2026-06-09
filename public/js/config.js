1d967b46295c31839e4c767b1bc0bfdacd64edf5
DavidNewman007 <4489530@gmail.com>
2026-06-09 12:36:55 +0300

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,22 @@
+window.IRON_CONFIG = {
+  apiUrl: "",
+  apiToken: "",
+  notifyPhone: "+79288509404",
+
+  // Прайс: ID ОТДЕЛЬНОЙ публичной таблицы (копия создаётся скриптом Price_DA-All)
+  // Доступ к этому файлу: «всем, у кого есть ссылка» → просмотр
+  googleSheetId: "11xhKh4rPN5XfZA7y8D14_rVxzT1dfvOjs_nv1HMSols",
+  googleSheetTab: "Prices",
+
+  // Куда уходит заказ из корзины (deep link ?text=)
+  telegramOrderUser: "ironsochi",
+
+  siteUrl: "https://ваш-домен.ru",
+
+  map: {
+    lat: 43.5852,
+    lon: 39.7231,
+    zoom: 17,
+    address: "Сочи, ул. Московская, 5 (цоколь, вход со двора ул. Островского)",
+  },
+};
